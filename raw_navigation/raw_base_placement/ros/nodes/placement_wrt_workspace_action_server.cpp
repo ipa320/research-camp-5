db5cdc34cc559f71cca957d787d8fc26beee16bb
Frederik Hegger <frederik.hegger@inf.h-brs.de>
2012-10-29 13:53:19 +0100

    bugfix

--- raw_navigation/raw_base_placement/ros/nodes/placement_wrt_workspace_action_server.cpp
+++ raw_navigation/raw_base_placement/ros/nodes/placement_wrt_workspace_action_server.cpp
@@ -133,8 +133,8 @@ public:
 
 		BaseScanLinearRegression srv;
 
-		srv.request.filter_minAngle = -M_PI_8;
-		srv.request.filter_maxAngle = M_PI_8;
+		srv.request.filter_minAngle = -M_PI / 8.0;
+		srv.request.filter_maxAngle = M_PI / 8.0;
 		srv.request.filter_minDistance = 0.02;
 		srv.request.filter_maxDistance = 0.80;
 
