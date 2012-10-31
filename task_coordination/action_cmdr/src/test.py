73578faadfbfdc8ba6322ef47e5a30e8f6b217d1
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2012-10-31 17:51:37 +0100

    action_cmdr: adapt test script to recent init changes
    Note: to actually use the test script you need to (temporarily!) add the
    action package, e.g. youbot_actions, as a dependency to the manifest
    file.

--- task_coordination/action_cmdr/src/test.py
+++ task_coordination/action_cmdr/src/test.py
@@ -3,8 +3,7 @@ import roslib
 roslib.load_manifest('action_cmdr')
 import rospy
 import action_cmdr
-action_cmdr.init(["youbot_actions"])
-
+action_cmdr.init("action_cmdr")
 
 if __name__ == "__main__":
     rospy.init_node('test') # needed to add this so I could make trajectory messages, require timestamp
