b617eef094a42f79461e3f03876c271c95a39c14
ipa-fxm <felix.messmer@ipa.fhg.de>
2012-10-31 22:16:21 +0100

    modified test-script

--- task_coordination/action_cmdr/src/test.py
+++ task_coordination/action_cmdr/src/test.py
@@ -10,15 +10,31 @@ if __name__ == "__main__":
     rospy.init_node('test') # needed to add this so I could make trajectory messages, require timestamp
     rospy.sleep(2)
     #action_cmdr.test("foo")
-    action_cmdr.move_arm(target="zeroposition", blocking=True)
+    #action_cmdr.move_arm(target="zeroposition", blocking=True)
+    action_cmdr.move_arm(target="wavein", blocking=True)
     pose = PoseStamped()
+
+### youbot
+    #pose.header.frame_id = "/base_link"
+    #pose.header.stamp = rospy.Time.now()
+    #pose.pose.position.x = 0.590
+    #pose.pose.position.y = -0.004
+    #pose.pose.position.z = 0.168
+    #pose.pose.orientation.x = 0
+    #pose.pose.orientation.y = 0
+    #pose.pose.orientation.z = 0
+    #pose.pose.orientation.w = 0
+    
+### cob
     pose.header.frame_id = "/base_link"
     pose.header.stamp = rospy.Time.now()
-    pose.pose.position.x = 0.590
-    pose.pose.position.y = -0.004
-    pose.pose.position.z = 0.168
-    pose.pose.orientation.x = 0
-    pose.pose.orientation.y = 0
-    pose.pose.orientation.z = 0
-    pose.pose.orientation.w = 0
+    pose.pose.position.x = -0.626
+    pose.pose.position.y = -0.005
+    pose.pose.position.z = 0.963
+    pose.pose.orientation.x = 0.225
+    pose.pose.orientation.y = 0.673
+    pose.pose.orientation.z = -0.664
+    pose.pose.orientation.w = -0.236   
+
     action_cmdr.move_arm(target=pose, blocking=True)
+
