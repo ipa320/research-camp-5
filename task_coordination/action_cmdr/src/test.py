b7aa0db1b1eb12a6b602a97fe7e53341b7c9c8d5
Leif Jentoft <ljentoft@seas.harvard.edu>
2012-10-31 17:40:10 +0100

    moved action_handle to "includable" location and updated test.py to include node being started

--- task_coordination/action_cmdr/src/test.py
+++ task_coordination/action_cmdr/src/test.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python
+import roslib; roslib.load_manifest('action_cmdr')
+import rospy 
+import time
 
 import action_cmdr
-action_cmdr.init(["youbot_actions"])
+action_cmdr.init(["cob_actions"])
 
 if __name__ == "__main__":
-    action_cmdr.test("foo")
+    rospy.init_node('test') # needed to add this so I could make trajectory messages, require timestamp
+    time.sleep(1)
+    action_cmdr.move("arm","home")
