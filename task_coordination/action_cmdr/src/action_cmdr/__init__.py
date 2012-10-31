f8b48467c195cd1cb1e7ab606049ec7976c7f4ea
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2012-10-31 17:50:21 +0100

    action_cmdr: simplify init function -- incompatible!
    This changes the init function to take the name of the current package
    rather than the list of action modules. We determine those by checking
    all dependencies of the current package for action exports. If they
    exist the module is automatically loaded.

--- task_coordination/action_cmdr/src/action_cmdr/__init__.py
+++ task_coordination/action_cmdr/src/action_cmdr/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+import roslib
 
 from plugins import load_module
 
@@ -23,11 +24,37 @@ class WrapModule(object):
 
 
 actions = DictObj()
+modules = []
 
 sys.modules[__name__] = WrapModule(sys.modules[__name__], actions)
 
-def init(module_names):
+def init(package_name):
+    #print("Initializing %s" % package_name)
+    to_check = roslib.rospack.rospack_depends_1(package_name)
+
+    action_modules = []
+
+    for pkg in to_check:
+        manifest_file = roslib.manifest.manifest_file(pkg, True)
+        manifest      = roslib.manifest.parse_file(manifest_file)
+
+        action_module_names = manifest.get_export('actions', 'action-modules')
+
+        if action_module_names:
+            for a in action_module_names:
+                action_modules += a.split(',')
+
+    #print("Action modules: %s" % action_modules)
+    for a in action_modules:
+        # Load that package's namespace
+        roslib.load_manifest(a)
+
+    load(action_modules)
+
+def load(module_names):
     global actions
+    global modules
     for m in module_names:
-        #print("Module: %s" % m)
-        load_module(m, actions)
+        if not m in modules:
+            modules.append(m)
+            load_module(m, actions)
