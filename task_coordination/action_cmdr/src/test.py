ed03284db575e2137976c3ee4f616cc2f73db059
Oliver Zendel <oliver.zendel@ait.ac.at>
2012-11-01 17:18:44 +0100

    Fixed test script manifest loading issue

--- task_coordination/action_cmdr/src/test.py
+++ task_coordination/action_cmdr/src/test.py
@@ -3,21 +3,26 @@ import roslib
 roslib.load_manifest('action_cmdr')
 import rospy
 import action_cmdr
-action_cmdr.init("action_cmdr")
+action_cmdr.load(["generic_actions"])
 from geometry_msgs.msg import PoseStamped
 
 if __name__ == "__main__":
     rospy.init_node('test') # needed to add this so I could make trajectory messages, require timestamp
     rospy.sleep(2)
+    
+    det_objects = action_cmdr.execute_perception(wait_time=1,retries=10,blocking=True)
+    if len(det_objects) > 0:
+        print("Got: %s",det_objects);
+
     #action_cmdr.test("foo")
     
-    action_cmdr.move_gripper(target="cylopen", blocking=True)
+    #action_cmdr.move_gripper(target="cylopen", blocking=True)
     
-    action_cmdr.move_torso(target="front", blocking=True)
+    #action_cmdr.move_torso(target="front", blocking=True)
     
-    action_cmdr.move_tray(target="down", blocking=True)
+    #action_cmdr.move_tray(target="down", blocking=True)
     
-    action_cmdr.move_head(target="front", blocking=True)
+    #action_cmdr.move_head(target="front", blocking=True)
     
     
     #action_cmdr.move_arm(target=["wavein","waveout","wavein"], blocking=True)
